aa4f6ad65731e32490a018456cd8db4b397848bb
MaksGMaks <maksgmaks.yt@gmail.com>
2024-10-11 20:43:29 +0300

    Ui is ready

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <QApplication>
+
+#include "MainWindow.hpp"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MainWindow *mainWindow = new MainWindow();
+    mainWindow->show();
+
+    return app.exec();
+}
